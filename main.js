7dbc9843149db5859e49a93cb7fcbb8a3df4e442
Kuda M'ndau <150191733+kudamndau@users.noreply.github.com>
2024-07-26 04:12:53 +0200

    feat(main): Add function to get the user's choice
    Added a function that prompts the user to input
    their choice, return the value and display it on the
    browser's console log.

--- main.js
+++ main.js
@@ -10,3 +10,20 @@ function getComputerChoice(items) {
 const choiceList = ["rock", "paper", "scissors"];
 const computerChoice = getComputerChoice(choiceList);
 console.log("Computer's choice is" + " " + computerChoice);
+
+// LOGIC TO GET USER CHOICE
+function getUserChoice() {
+  // Prompt the user to input their choice
+  const userInput = prompt("Welcome to Rock, Paper, Scissors. What's your choice?");
+  const userChoice = userInput("rock" OR, "paper" OR, "scissors");
+  
+  if (isString(userChoice)) {
+    console.log("Your choice is" + " " + userChoice);
+  } else {
+    console.log("Invalid input. Please make a choice")
+  } 
+
+  return userChoice;
+}
+
+getUserChoice()
